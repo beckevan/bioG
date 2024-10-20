a2cca801ebe65dfebef87e3e5bb77c9743d0c72a
eb-10 <beckevan64@gmail.com>
2024-10-20 16:11:14 -0500

    new file:   main/src/assets/heavy_forest_tile_1.png 	new file:   main/src/assets/heavy_forest_tile_2.png 	new file:   main/src/assets/heavy_forest_tile_3.png 	new file:   main/src/assets/heavy_forest_tile_4.png 	modified:   main/src/game_init.py 	modified:   main/src/main.py
    Added temporary textures, progress on main game

--- main/src/game_init.py
+++ main/src/game_init.py
@@ -1,32 +1,60 @@
 import pygame
 
 class GameInitializer:
-    def __init__(self, car_path, cassowary_path):
+    def __init__(
+            self, 
+            car_path, 
+            cassowary_path, 
+            heavy_forest_tile_1_path, 
+            heavy_forest_tile_2_path, 
+            heavy_forest_tile_3_path, 
+            heavy_forest_tile_4_path, 
+            window_size):
         self.cassowary_unscaled = None
         self.car_unscaled = None
+        self.heavy_forest_tile_1_unscaled = None
+        self.heavy_forest_tile_2_unscaled = None
+        self.heavy_forest_tile_3_unscaled = None
+        self.heavy_forest_tile_4_unscaled = None
         self.lane_lines = None
         self.lane_amount = 4
+        self.tiles = []
         self.car_path = car_path
         self.cassowary_path = cassowary_path
+        self.heavy_forest_tile_1_path = heavy_forest_tile_1_path
+        self.heavy_forest_tile_2_path = heavy_forest_tile_2_path
+        self.heavy_forest_tile_3_path = heavy_forest_tile_3_path
+        self.heavy_forest_tile_4_path = heavy_forest_tile_4_path
         self.init_assets()
-        self.init_game_vars()  # Call to initialize game variables here
+        self.init_game_vars(window_size[1])
+        self.init_background(window_size)
 
     def init_assets(self):
         # Load images using the provided paths
         self.cassowary_unscaled = pygame.image.load(self.cassowary_path)
         self.car_unscaled = pygame.image.load(self.car_path)
+        self.heavy_forest_tile_1_unscaled = pygame.image.load(self.heavy_forest_tile_1_path)
+        self.heavy_forest_tile_2_unscaled = pygame.image.load(self.heavy_forest_tile_2_path)
+        self.heavy_forest_tile_3_unscaled = pygame.image.load(self.heavy_forest_tile_3_path)
+        self.heavy_forest_tile_4_unscaled = pygame.image.load(self.heavy_forest_tile_4_path)
 
-    def init_game_vars(self):
+    def init_game_vars(self, window_y):
         # Initialize game variables
-        self.lane_lines = [((1000 // 2, 0), (1000 // 2, 600))]  # Use fixed values here or pass window size as an argument
+        self.lane_lines = [((1000 // 2, 0), (1000 // 2, window_y))]  # Use fixed values here or pass window size as an argument
 
         for i in range(1, round(self.lane_amount / 2) + 1):
             self.lane_lines.append(
-                ((1000 // 2, 0), (1000 // 2 - (i * 200), 600))
+                ((1000 // 2, 0), (1000 // 2 - (i * 200), window_y))
             )
             self.lane_lines.append(
-                ((1000 // 2, 0), (1000 // 2 + (i * 200), 600))
+                ((1000 // 2, 0), (1000 // 2 + (i * 200), window_y))
             )
 
         self.lane_lines = sorted(self.lane_lines, key=lambda x: x[1][0])
-        print(self.lane_lines)
+
+    def init_background(self, window_size):
+        self.tile_textures = [[],[],[],[]]
+        self.tile_amount = (window_size[0] // 128 + 1, window_size[1] // 128 + 1)
+        for x in range(0, self.tile_amount[0]):
+            for y in range(0, self.tile_amount[1]):
+                self.tiles.append((x * 128, y * 128))
--- main/src/main.py
+++ main/src/main.py
@@ -1,5 +1,7 @@
 import pygame
 import sys
+import random
+import threading
 from game_init import GameInitializer
 
 # Initialize Pygame and font
@@ -10,7 +12,7 @@ pygame.font.init()
 def screen_init():
     global screen, window_size, clock
 
-    window_size  = (1000, 600)
+    window_size  = (1024, 640)
     icon_path = r'main\src\assets\icon.png'
 
     screen = pygame.display.set_mode(window_size)
@@ -28,17 +30,35 @@ class Game:
     class assets:
         car_path = r'main\src\assets\car.png'
         cassowary_path = r'main\src\assets\cassowary.png'
+        heavy_forest_tile_1_path = r'main\src\assets\heavy_forest_tile_1.png'
+        heavy_forest_tile_2_path = r'main\src\assets\heavy_forest_tile_2.png'
+        heavy_forest_tile_3_path = r'main\src\assets\heavy_forest_tile_3.png'
+        heavy_forest_tile_4_path = r'main\src\assets\heavy_forest_tile_4.png'
 
     def __init__(self):
-        self.initializer = GameInitializer(self.assets.car_path, self.assets.cassowary_path)
+        self.initializer = GameInitializer(
+            self.assets.car_path, 
+            self.assets.cassowary_path, 
+            self.assets.heavy_forest_tile_1_path,
+            self.assets.heavy_forest_tile_2_path,
+            self.assets.heavy_forest_tile_3_path,
+            self.assets.heavy_forest_tile_4_path,
+            window_size)
         self.car_sprite = pygame.transform.scale2x(self.initializer.car_unscaled)
+        self.heavy_forest_tile_1 = pygame.transform.scale_by(self.initializer.heavy_forest_tile_1_unscaled, 4)
+        self.heavy_forest_tile_2 = pygame.transform.scale_by(self.initializer.heavy_forest_tile_2_unscaled, 4)
+        self.heavy_forest_tile_3 = pygame.transform.scale_by(self.initializer.heavy_forest_tile_3_unscaled, 4)
+        self.heavy_forest_tile_4 = pygame.transform.scale_by(self.initializer.heavy_forest_tile_4_unscaled, 4)
+        self.tile_textures = self.initializer.tile_textures
         self.lane_lines = self.initializer.lane_lines
+        self.lane_amount = self.initializer.lane_amount
+        self.tiles = self.initializer.tiles
         self.car = self.Car()
         self.is_game_started = False
 
     class Car:
         def __init__(self):
-            self.lane = 1
+            self.lane = 0
             self.pos = None
 
     def boot(self):
@@ -49,6 +69,17 @@ class Game:
         screen.blit(start_button, (200, 80))
 
     def start(self):
+        for tile in self.tiles:
+            self.tile_textures[random.randint(0,3)].append(tile)
+        for tile in self.tile_textures[0]:
+            screen.blit(self.heavy_forest_tile_1, tile)
+        for tile in self.tile_textures[1]:
+            screen.blit(self.heavy_forest_tile_2, tile)
+        for tile in self.tile_textures[2]:
+            screen.blit(self.heavy_forest_tile_3, tile)
+        for tile in self.tile_textures[3]:
+                screen.blit(self.heavy_forest_tile_4, tile)
+
         self.is_game_started = True
         screen.fill((255, 255, 255))
         self.car.pos = (self.lane_lines[self.car.lane][1][0] + 60, window_size[1] - 72)
@@ -59,11 +90,18 @@ class Game:
     def update(self):
         if self.is_game_started:
             screen.fill((255, 255, 255))
+            for tile in self.tile_textures[0]:
+                screen.blit(self.heavy_forest_tile_1, tile)
+            for tile in self.tile_textures[1]:
+                screen.blit(self.heavy_forest_tile_2, tile)
+            for tile in self.tile_textures[2]:
+                screen.blit(self.heavy_forest_tile_3, tile)
+            for tile in self.tile_textures[3]:
+                screen.blit(self.heavy_forest_tile_4, tile)
             for line in self.lane_lines:
                 pygame.draw.line(screen, (0, 0, 0), line[0], line[1], 1)
             self.car.pos = (self.lane_lines[self.car.lane][1][0] + 60, window_size[1] - 72)
             screen.blit(self.car_sprite, self.car.pos)
-
 
 # Initialize game
 game = Game()
@@ -78,9 +116,13 @@ while running:
             sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
-            print(pos)
             if pos[0] >= 200 and pos[0] <= 360 and pos[1] >= 80 and pos[1] <= 120:
                 game.start()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT and game.car.lane < game.lane_amount - 1:
+                game.car.lane += 1
+            elif event.key == pygame.K_LEFT and game.car.lane > 0:
+                game.car.lane -= 1
 
     game.update()
 
